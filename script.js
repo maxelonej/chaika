4415c4e2f73cd35da55f860295b21d2b1929aba5
Maxelone <maxelone@yandex.ru>
2025-03-15 17:50:55 +0400

    Add second page

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const navLinks = document.querySelector(".burger-links");
 burgerMenu?.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+// Фильтрация категорий
+document.querySelectorAll("#categoryFilters .btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document
+      .querySelector("#categoryFilters .active")
+      .classList.remove("active");
+    btn.classList.add("active");
+
+    const category = btn.dataset.category;
+    document.querySelectorAll(".menu-category").forEach((section) => {
+      section.style.display =
+        category === "all" || section.dataset.category === category
+          ? "block"
+          : "none";
+    });
+  });
+});
